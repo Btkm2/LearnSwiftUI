ce98291b366c1d898035a525299de706173a51c1
Btkm2 <beketmuratbek57@gmail.com>
2022-11-25 14:29:40 +0600

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Beket Muratbek on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

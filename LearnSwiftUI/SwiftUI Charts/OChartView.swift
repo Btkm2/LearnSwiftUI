5af2a43b1eb3032bc5703fd916064817f7c60d8e
Btkm2 <beketmuratbek57@gmail.com>
2024-02-28 20:19:56 +0600

    Playing with charts

---
+++ LearnSwiftUI/SwiftUI Charts/OChartView.swift
@@ -0,0 +1,18 @@
+//
+//  OChartView.swift
+//  LearnSwiftUI
+//
+//  Created by Beket Muratbek on 28.02.2024.
+//
+
+import SwiftUI
+
+struct OChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OChartView()
+}
---
+++ LearnSwiftUI/UIKitSwiftUIPreviews/UIKitSwiftUIPreviewView.swift
@@ -0,0 +1,20 @@
+//
+//  UIKitSwiftUIPreviewView.swift
+//  LearnSwiftUI
+//
+//  Created by Beket Muratbek on 14.05.2023.
+//
+
+import SwiftUI
+
+struct UIKitSwiftUIPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UIKitSwiftUIPreviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitSwiftUIPreviewView()
+    }
+}
